3cfacc5c07b7174a2b61a18872cca96524e016ea
Luciano Tadeu <lucianotssjr@gmail.com>
2026-07-09 09:44:44 -0400

    Delete IniciarSistema.bat

--- IniciarSistema.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "C:\Program Files\Eclipse Adoptium\jdk-25.0.2.10-hotspot\bin\java.exe" --module-path "C:\java\javafx-sdk-26.0.1\lib" --add-modules javafx.controls,javafx.fxml -jar Sistema-Controle-de-Acesso.jar
